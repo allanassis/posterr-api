7a42003a07ccc746c3255a65046383ce66bde734
Allan Assis <allan-bdc@hotmail.com>
2022-05-12 00:49:29 -0300

    Create save method in database class

--- posterr/storages/database.py
+++ posterr/storages/database.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 from typeguard import typechecked
 
 from pymongo.mongo_client import MongoClient
@@ -11,6 +12,12 @@ class DataBase:
     def __init__(self, name:str, host:str, port:int):
         self.client:MongoClient = MongoClient(host, port)
         self.db = self.client[name]
+
+    def save(self, item:object):
+        item_dict:dict = dict(item)
+        collection_name:str = format(item)
+        result = self.db[collection_name].insert_one(item_dict)
+        return str(result.inserted_id)
 
     def healthcheck(self):
         info:dict = self.client.server_info()

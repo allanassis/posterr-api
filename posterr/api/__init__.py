63d260dc4f2b3b7bd3f1230a7d1ca78f1c8b178c
Allan Assis <allan-bdc@hotmail.com>
2022-05-12 00:38:18 -0300

    Get database name from config to instance the database

--- posterr/api/__init__.py
+++ posterr/api/__init__.py
@@ -8,11 +8,12 @@ from posterr.storages.database import DataBase
 
 @typechecked
 def init_api(config: Vyper):
-    host:str = config.get_string("storages.database.host")
-    port:str = config.get_int("storages.database.port")
+    db_host:str = config.get_string("storages.database.host")
+    db_port:str = config.get_int("storages.database.port")
+    db_name:str = config.get_string("storages.database.name")
 
     app:Application = Application(middlewares=[web.normalize_path_middleware()])
-    app["db"] = DataBase(host, port)
+    app["db"] = DataBase(db_name, db_host, db_port)
     app.add_routes([web.get('/healthcheck', healthcheck)])
 
     web.run_app(app)
--- posterr/storages/database.py
+++ posterr/storages/database.py
@@ -1,13 +1,16 @@
 from typeguard import typechecked
 
 from pymongo.mongo_client import MongoClient
+from pymongo.database import Database
 
 @typechecked
 class DataBase:
     client: MongoClient
+    db: Database
 
-    def __init__(self, host:str, port:int):
+    def __init__(self, name:str, host:str, port:int):
         self.client:MongoClient = MongoClient(host, port)
+        self.db = self.client[name]
 
     def healthcheck(self):
         info:dict = self.client.server_info()

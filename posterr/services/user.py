f3e75623d963a8c748074ffe68af46958cad8d16
Allan Assis <allan-bdc@hotmail.com>
2022-05-13 01:53:16 -0300

    Create User service to handle user logic

---
+++ posterr/services/user.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import typing
+
+from bson import ObjectId
+
+from posterr.storages.dao.user import UserDao
+from posterr.storages.database import DataBase
+from posterr.services.post import Post
+
+class User:
+    _id: ObjectId
+    name: str
+    created_at: datetime
+    followers: typing.Dict[int, typing.List[ObjectId]]
+    following:typing.Dict[int, typing.List[ObjectId]]
+    posts: typing.Dict[int, typing.List[ObjectId]]
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.created_at = datetime.now().isoformat()
+        self.followers = {"count": 0, "list": []}
+        self.following = {"count": 0, "list": []}
+        self.posts = {"count": 0, "list": []}
+
+    def save(self, db:DataBase) -> ObjectId:
+        inserted_id = db.save(self, User.__name__.lower())
+        return ObjectId(inserted_id)
+
+    @staticmethod
+    def get(id: str, db: DataBase) -> User:# type: ignore
+        item = db.get(id, User.__name__.lower())
+        user = User()
+        return user.build(item)
+
+    def build(self, properties:dict) -> User:# type: ignore
+        for k,v in enumerate(properties):
+            setattr(self, k, v)
+        return self

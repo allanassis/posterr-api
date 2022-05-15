48a76854a97554f41f242b5ffc5c2852b31c7bc5
Allan Assis <allan-bdc@hotmail.com>
2022-05-15 19:30:20 -0300

    Refactor code of post handle to be more readable

--- posterr/api/handlers/post.py
+++ posterr/api/handlers/post.py
@@ -1,55 +1,69 @@
-from aiohttp.web import View, Response, HTTPOk
-from typeguard import typechecked
+from typing import List
 
-from posterr.services.post import Post, PostType
+from typeguard import typechecked
+from aiohttp.web import View, Response, HTTPOk
+from aiohttp.typedefs import _MultiDictProxy
+
 from posterr.services.user import User
-from posterr.api.handlers.base import BaseHandler
+from posterr.services.post import Post, PostType
 from posterr.storages.dao.user import UserDao
 from posterr.storages.database import DataBase
 from posterr.storages.dao.post import PostDao
+from posterr.api.handlers.base import BaseHandler
 
 @typechecked
 class PostHandlers(BaseHandler, View):
 
     async def get(self) -> Response:
-        id:str = self.request.match_info.get('id')
+        post_id:str = self.request.match_info.get('id')
         db:DataBase = self.request.config_dict["db"]
-        last_post_date = self.request.query.get("last_post_date", "")
-        limit_per_page = int(self.request.query.get("limit", 10))
-        user_id = self.request.query.get("user_id")
 
-        userDao = UserDao()
-        user:User = None
+        [ user_id, last_post_date, limit_per_page ] = self._get_queries(self.request.query)
 
-        dao = PostDao({
-            "user_id": id,
-            "limit": limit_per_page,
+        post_dao_args:dict = {
+            "limit": int(limit_per_page),
             "last_post_date": last_post_date,
-        })
+        }
 
         if user_id:
-            user = User.get_by_id(user_id, userDao, db)
-            dao.queries["following_list"] = user.following["list"]
+            user_dao:UserDao = UserDao()
+            user:User = User.get_by_id(user_id, user_dao, db)
+            post_dao_args["following_list"] = user.following["list"]
 
-        if id is not None:
-            return await self.get_by_id(id, Post, dao, db)
+        post_dao:PostDao = PostDao(post_dao_args)
+        if post_id is not None:
+            return await self.get_by_id(post_id, Post, post_dao, db)
 
-        return await self.get_all(Post, dao, db)
+        return await self.get_all(Post, post_dao, db)
+
+    def _get_queries(self, query:_MultiDictProxy) -> List[str]:
+        queries:List = [
+            query.get("user_id", ""),
+            query.get("last_post_date", ""),
+            query.get("limit", "10")
+        ]
+        return queries
 
     async def post(self) -> Response:
         body:dict = await self.request.json()
         db: DataBase = self.request.config_dict["db"]
 
-        user_id:str = body.get("user_id")
-        type:str = body.get("type", PostType.NORMAL.name)
-        parent_id:str = body.get("parent_id")
-        text:str = body.get("text")
+        [ user_id, post_parent_id, post_type, post_text ] = self._get_body(body)
 
-        userDao = UserDao()
+        userDao:UserDao = UserDao()
         user:User = User.get_by_id(user_id, userDao, db)
 
-        postDao = PostDao()
-        post:Post = Post(text, user_id, parent_id, type)
+        postDao:PostDao = PostDao()
+        post:Post = Post(post_text, user_id, post_parent_id, post_type)
         post_id:str = user.post(post, userDao, postDao, db)
 
         return Response(body=post_id, status=HTTPOk.status_code)
+
+    def _get_body(self, body:dict) -> List[str]:
+        body:List = [
+            body.get("user_id", ""),
+            body.get("parent_id", ""),
+            body.get("type", PostType.NORMAL.name),
+            body.get("text", "")
+        ]
+        return body

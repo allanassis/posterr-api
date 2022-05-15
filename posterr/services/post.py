c9aba03598b8f48cc00517eb197e07cf91a48b2d
Allan Assis <allan-bdc@hotmail.com>
2022-05-15 19:50:32 -0300

    Get date format and post maximum size from config

--- posterr/services/post.py
+++ posterr/services/post.py
@@ -1,10 +1,10 @@
 import json
-from datetime import datetime
 from enum import Enum
+from datetime import datetime
 
 from typeguard import typechecked
 
-from posterr.storages.database import DataBase
+from posterr.config import ConfigManager
 from posterr.services.base import ServiceBase
 
 class PostType(Enum):
@@ -24,19 +24,24 @@ class Post(ServiceBase):
     entity_name:str = "post"
 
     def __init__(self, text: str = None, user_id: str = None, parent_id: str = None, type:str = PostType.NORMAL.name) -> None:
-        if text is not None and len(text) > 777:
-            raise ValueError("Post has a maximum lenght of 777")
-        self.text = text
-        self.user_id = user_id
+        maxium_size:int = ConfigManager().config.get_int("post.maximum_size")
+
+        if text is not None and len(text) > maxium_size:
+            raise ValueError(f"Post has a maximum lenght of {maxium_size}")
         if parent_id is not None:
             self.parent_id = parent_id
         if type is not None:
             self.type = PostType[type].value
+
+        self.text = text
+        self.user_id = user_id
         self.created_at = datetime.now()
     
     def __str__(self) -> str:
+        date_format:int = ConfigManager().config.get_int("post.date_format")
         post_dict:dict = self.__dict__
-        type:PostType = post_dict.pop("type")
-        created_at:str = post_dict.pop("created_at").strftime("%b %d, %Y")
 
-        return json.dumps({**self.__dict__, "type": type, "created_at": created_at})
+        type:PostType = post_dict.pop("type")
+        created_at:str = post_dict.pop("created_at").strftime(date_format) # ex May 24, 2021
+
+        return json.dumps({ **self.__dict__, "type": type, "created_at": created_at })

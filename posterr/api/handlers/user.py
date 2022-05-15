5039c2d108a88790b48773ad941f83115c2bbf16
Allan Assis <allan-bdc@hotmail.com>
2022-05-15 19:40:19 -0300

    Refactor code of user handle to be more readable

--- posterr/api/handlers/user.py
+++ posterr/api/handlers/user.py
@@ -1,51 +1,58 @@
-from aiohttp.web import Response, View, HTTPOk
+from typing import List
+
 from typeguard import typechecked
+from aiohttp.web import Response, View, HTTPOk
 
 from posterr.services.user import User
-from posterr.api.handlers.base import BaseHandler
 from posterr.storages.dao.user import UserDao
 from posterr.storages.database import DataBase
+from posterr.api.handlers.base import BaseHandler
 
 @typechecked
 class UserHandlers(BaseHandler, View):
 
     async def get(self) -> Response:
-        id:str = self.request.match_info.get('id')
+        user_id:str = self.request.match_info.get('id')
         db:DataBase = self.request.config_dict["db"]
-        dao = UserDao()
 
-        if id is not None:
-            return await self.get_by_id(id, User, dao, db)
+        user_dao:UserDao = UserDao()
 
-        return await self.get_all(User, dao, db)
+        if user_id is not None:
+            return await self.get_by_id(user_id, User, user_dao, db)
+
+        return await self.get_all(User, user_dao, db)
 
     async def post(self) -> Response:
         body:dict = await self.request.json()
-        user:User = User(name=body["name"])
-        dao = UserDao()
-        user_id:str = user.save(dao, self.request.config_dict["db"])
+        db:DataBase = self.request.config_dict["db"]
+
+        user_dao:UserDao = UserDao()
+        user:User = User(name=body.get("name", ""))
+        user_id:str = user.save(user_dao, db)
 
         return Response(body=user_id, status=HTTPOk.status_code)
 
     async def put(self) -> Response:
+        user_id:str = self.request.match_info.get('id')
         body:dict = await self.request.json()
         db:DataBase = self.request.config_dict["db"]
-        userDao = UserDao()
-        id:str = self.request.match_info.get('id')
-        action:str = body.get("action")
-        following_id:str = body.get("following")
-        user:User = User(id, body.get("name"))
+
+        [ action, following_id, user_name ] = self._get_body(body)
+        user_dao:UserDao = UserDao()
+        user:User = User(user_id, user_name)
 
         if action == "FOLLOW":
-            user.follow(following_id, userDao, db)
+            user.follow(following_id, user_dao, db)
+
         elif action == "UNFOLLOW":
-            user.unfollow(following_id, userDao, db)
+            user.unfollow(following_id, user_dao, db)
 
-        return Response(body=id, status=HTTPOk.status_code)
+        return Response(body=user_id, status=HTTPOk.status_code)
 
-    # async def delete(self):
-    #     id = self.request.match_info.get('id')
-    #     db = self.request.config_dict["db"]
-
-    #     deleted = User.delete(id, db)
-    #     return web.Response(body=str(deleted), status=web.HTTPOk.status_code)
+    def _get_body(self, body:dict) -> List[str]:
+        body = [
+            body.get("action", ""),
+            body.get("following", ""),
+            body.get("name", "")
+        ]
+        return body

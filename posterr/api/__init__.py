4287baea28dd2d891f98e1968837d32bbe128fec
Allan Assis <allan-bdc@hotmail.com>
2022-05-16 22:10:03 -0300

    Add middlewares to handle error, timeout and cors

--- posterr/api/__init__.py
+++ posterr/api/__init__.py
@@ -2,7 +2,7 @@ import logging
 from typeguard import typechecked
 from aiohttp.web import Application
 from aiohttp import web
-from vyper import Vyper
+from aiohttp_middlewares import error, cors, timeout
 
 from posterr.api.handlers.healthcheck import healthcheck
 from posterr.api.handlers.post import PostHandlers
@@ -16,10 +16,17 @@ def init_api() -> None:
 
     config = ConfigManager().config
     db_host:str = config.get_string("storages.database.host")
-    db_port:str = config.get_int("storages.database.port")
+    db_port:int = config.get_int("storages.database.port")
     db_name:str = config.get_string("storages.database.name")
 
-    app:Application = Application(middlewares=[web.normalize_path_middleware()])
+    timeout_time:int = config.get_int("storages.database.port")
+
+    app:Application = Application(middlewares=[
+        web.normalize_path_middleware(),
+        error.error_middleware(),
+        cors.cors_middleware(allow_all=True), # TODO: THIS SHOULD BE CHANGE TO THE SPA DOMAIN THAT WILL USE THIS API
+        timeout.timeout_middleware(timeout_time)
+    ])
     app["db"] = DataBase(db_name, db_host, db_port)
 
     app.add_routes([web.get('/healthcheck', healthcheck)])
